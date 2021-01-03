7618daa73987b1e2e373a43bf02f1e8c86031971
Fujishima-Kai <fujishima.ki@gmail.com>
2021-01-03 16:36:21 +0900

    new change

--- resources/views/media/newsletter/partials/main.blade.php
+++ resources/views/media/newsletter/partials/main.blade.php
@@ -85,9 +85,9 @@
 			<td colspan="3" width="560" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
 
 				<!--▼▼ テキスト ▼▼-->
-				<p style="font-size:16px;color:#555555;text-align:center;line-height:1.7;">
+				<p style="font-size:16px;color:#555555;text-align:center;line-height:1.7;"><center>
 				{!! $np->text !!}
-				</p>
+				</center></p>
 				<!--▲▲ テキスト ▲▲-->
 
 			</td>
