9a906493fb79f319c68bf64ef88504bdd1c22644
Fujishima-Kai <fujishima.ki@gmail.com>
2021-10-16 15:29:31 +0900

    change footer

---
+++ resources/views/media/newsletter/partials/footertocopylight.blade.php
@@ -0,0 +1,117 @@
+<table width="600" border="0" cellpadding="0" cellspacing="0" align="center">
+	<tbody>
+		<tr>
+		<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top"><a href="https://www.familiar.co.jp/index.html?utm_source=h_mail&amp;utm_medium=email&amp;utm_content=footer" target="_blank" style="margin:0;padding:0;vertical-align:top;font-size:0;text-decoration:none;"><img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/btn_officialsite.png" width="184" height="56" border="0" style="display:inline-block;"></a></td>
+		<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top"><img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_24x24.gif" width="24" height="24" border="0" style="display:block;"></td>
+		<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top"><a href="https://www.ec.familiar.co.jp?utm_source=h_mail&amp;utm_medium=email&amp;utm_content=footer" target="_blank" style="margin:0;padding:0;vertical-align:top;font-size:0;text-decoration:none;"><img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/btn_onlineshop.png" width="184" height="56" border="0" style="display:inline-block;"></a></td>
+		<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top"><img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_24x24.gif" width="24" height="24" border="0" style="display:block;"></td>
+		<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top"><a href="https://blog.familiar.co.jp/brand/familiar?utm_source=h_mail&amp;utm_medium=email&amp;utm_content=footer" target="_blank" style="margin:0;padding:0;vertical-align:top;font-size:0;text-decoration:none;"><img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/btn_blog.png" width="184" height="56" border="0" style="display:inline-block;"></a></td>
+		</tr>
+		<tr><td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top"><img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/line_600x82.gif" width="600" height="82" border="0" style="display:block;"></td></tr>
+	</tbody>
+</table>
+	
+	
+	<table width="600" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="f7f5f1">
+		<tbody>
+		<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top"><img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_600x32.gif" width="600" height="32" border="0" style="display:block;"></td></tr>
+			<tr>
+				<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_225x58.gif" width="225" height="58" border="0" style="display:block;">
+				</td>
+				<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<a href="https://page.line.me/317chcid?openQrModal=true?utm_source=h_mail&amp;utm_medium=email&amp;utm_content=footer" target="_blank" style="margin:0;padding:0;vertical-align:top;font-size:0;text-decoration:none;">
+						<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/LINE_APP.png" width="58" height="58" border="0" style="display:inline-block;">
+					</a><i class="fab fa-line"></i>
+				</td>
+
+				<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_34x34.gif" width="34" height="34" border="0" style="display:block;">
+				</td>
+				<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<a href="https://www.instagram.com/familiar_official?utm_source=h_mail&amp;utm_medium=email&amp;utm_content=footer" target="_blank" style="margin:0;padding:0;vertical-align:top;font-size:0;text-decoration:none;">
+						<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/Instagram_AppIcon_Aug2017.png" width="58" height="58" border="0" style="display:inline-block;">
+					</a>
+				</td>
+				<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_225x58.gif" width="225" height="58" border="0" style="display:block;">
+				</td>
+			</tr>
+
+
+
+			@if($newsletter->category == "本会員")<!-- 本会員 -->
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_600x28.gif" width="600" height="28" border="0" style="display:block;">
+				</td>
+			</tr>
+			
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<p style="font-size:14px;color:#555555;text-align:center;">メール配信停止をご希望の方は
+					<a href="https://www.ec.familiar.co.jp/mypage/login.php?utm_source=h_mail&amp;utm_medium=email&amp;utm_content=footer" target="_blank">こちら</a>から</p>
+				</td>
+			</tr>
+
+			
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_600x8.gif" width="600" height="8" border="0" style="display:block;">
+				</td>
+			</tr>
+			@elseif($newsletter->category == "NL会員")<!-- NL会員 -->
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_600x28.gif" width="600" height="28" border="0" style="display:block;">
+				</td>
+			</tr>
+			
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<p style="font-size:14px;color:#555555;text-align:center;">
+					<pre><code>___BLOCK(cus_yobi001 = 本会員)___
+メール配信停止をご希望の方は</code></pre><a href="https://www.ec.familiar.co.jp/mypage/login.php?utm_source=h_mail&utm_medium=email&utm_content=footer" target="_blank">こちら</a>から
+<pre><code>___BLOCKEND___</code></pre>
+
+<pre><code>___BLOCK(cus_yobi001 = NL)___
+メール配信停止をご希望の方は</code></pre><a href="https://www.ec.familiar.co.jp/entry/mailmaga.php?mrf=2?utm_source=h_mail&utm_medium=email&utm_content=footer" target="_blank">こちら</a>から
+<pre><code>___BLOCKEND___</code></pre>/p>
+				</td>
+			</tr>
+
+			
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_600x8.gif" width="600" height="8" border="0" style="display:block;">
+				</td>
+			</tr>
+			@endif
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+				<p style="font-size:14px;color:#555555;text-align:center;">ファミリアへのお問い合わせは
+				<a href="https://www.ec.familiar.co.jp/contact/index.php?utm_source=h_mail&amp;utm_medium=email&amp;utm_content=footer" target="_blank">こちら</a>から</p>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="5" style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/spacer_600x20.gif" width="600" height="20" border="0" style="display:block;">
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	
+	
+	<table width="600" border="0" cellpadding="0" cellspacing="0" align="center">
+		<tbody>
+			<tr>
+				<td style="margin:0;padding:0;vertical-align:top;font-size:0;" valign="top">
+					<img src="https://www.ec.familiar.co.jp/user_data/packages/mail/content/2020/copyright.gif" width="600" height="32" border="0" style="display:block;">
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+</body>
+</html>
